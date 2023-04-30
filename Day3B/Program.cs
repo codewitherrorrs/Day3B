816ac56f76a38b2a6b5d1adb857ab0026ca0f085
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-30 10:49:56 +0530

    Add : Types of Constructor

---
+++ Day3B/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day3B
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TypesOfConstructor obj = new TypesOfConstructor();
+            TypesOfConstructor obj_One = new TypesOfConstructor("Sagar","RFP",85,"Pass");
+            TypesOfConstructor obj_Two = new TypesOfConstructor("Mustang");
+            Console.WriteLine($"Brand Name : {obj_Two.brand}");
+            //Copy Constructor
+            TypesOfConstructor construct = new TypesOfConstructor(obj_Two);
+            Console.WriteLine("Copy Constructor : "+construct.brand);
+            Console.WriteLine("Default Constructor : "+obj_Two.presentDay);
+            obj_One.ConstructorOutput();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day3B/TypesOfConstructor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B
+{
+    public class TypesOfConstructor
+    {
+       public string name,batch,result,brand;
+        int marks;
+        public int presentDay = 50; 
+        //Normal Constructor
+        public TypesOfConstructor() 
+        {
+            Console.WriteLine($"This is Normal Constructor.....");
+        }
+        //Parameterized Constructor
+        public TypesOfConstructor(string name,string batch,int marks, string result)
+        {
+            this.name = name;
+            this.batch = batch;
+            this.marks = marks;
+            this.result = result;
+        }
+        //To get the Output of the Parameterized Constructor.
+        public void ConstructorOutput()
+        {
+            Console.WriteLine("Name : "+name);
+            Console.WriteLine("Batch : "+batch);
+            Console.WriteLine("Marks : "+marks);
+            Console.WriteLine("Result : "+result);
+        }
+        public TypesOfConstructor(string thebrand)
+        {
+            brand = thebrand;        
+        }
+        //Copy Constructor
+        public TypesOfConstructor(TypesOfConstructor construct)
+        {
+                brand = construct.brand;
+        }
+    }
+}
